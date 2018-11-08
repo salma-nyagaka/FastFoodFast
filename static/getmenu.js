bd07b444c2500ecf7268074d02bfc95b1255c643
salma nyagaka <salmanyagaka@gmail.com>
2018-11-08 20:45:15 +0300

    [WIP]

--- static/getmenu.js
+++ static/getmenu.js
@@ -1,4 +1,5 @@
 //function to login
+
 window.onload = function(){
 
     //passing a keyName that returns a key's value or returns null if the key does not exist
@@ -21,11 +22,19 @@ window.onload = function(){
                 }
     })
      
-    // JSON extracts the JSON body content from the response
+    // JSON extracts the JSON body conteznt from the response
     .then(res=>res.json())
-    .then(data =>{ 
-
+    .then(data =>{              
+        
         if(data['message'] === "These are meals") {
+            elem = document.getElementById('dialog');
+            elem.classList.remove('hidden');
+            let element = document.createElement('h2')
+            element.innerHTML =  "These are the available meals";
+            element.id = "theoutput"
+            elem.appendChild(element)
+            
+
         let output = '';
         data["Food menu"].forEach(res=>{
             output +=` 
@@ -35,22 +44,23 @@ window.onload = function(){
                                         <h2 id="menuname">${res['name']}</h2>
                                         <p id="menudescription">${res['description']}</p>
                                         <h2 id="menuprice">${res['price']}</h2>  
+                                        <button class="ORDER"  onClick="getMeal('${res['id']}')">EDIT</button>
+                                        <br><br>
                                         <button class="ORDER"  onClick="deleteMeal('${res['id']}')">DELETE</button>
                                     </div>
                             </div>`
                        
         }) 
         document.getElementById("container").innerHTML = output;
-
-    }
+       
+              }
         else{
-            let displayWindow = document.getElementById('dialog')
             elem = document.getElementById('dialog');
-            displayWindow.classList.remove('hidden');
+            elem.classList.remove('hidden');
             let element = document.createElement('p')
             element.innerHTML =  `${data["message"]}`;
             element.id = "theoutput"
-            document.getElementById('dialog').appendChild(element)
+            elem.appendChild(element)
 
             if (window.localStorage.getItem('username') == null){
                 elem = document.getElementById('dialog');
@@ -67,10 +77,87 @@ window.onload = function(){
         console.log(error)            
             })
     }
+
+function getMeal(id){
+      
+    let qInputDiv = document.getElementById("food");
+           
+    qInputDiv.classList.remove("hidden")
+    let qInput = document.getElementById("quantity");
+    updateMeal(id)
+
+}
+
+function closeDialog() {
+    let qInputDiv = document.getElementById("food")
+ 
+    qInputDiv.classList.add("hidden")
+} 
+
+function updateMeal(id){
+
+    var newfood = document.getElementById('update')
+    newfood.addEventListener("click", () =>{
+    let name = document.getElementById('name').value;
+    let description = document.getElementById('description').value;
+    let price = document.getElementById('price').value;
+    let image = document.getElementById('images').value;
+
+    fetch(`http://127.0.0.1:5000/api/v2/menu/${id}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : 'Bearer ' + window.localStorage.getItem('token')
+        },
+
+        //declaring content type
+        body: JSON.stringify({
+            "name": name,
+            "description": description,
+            "price": price,
+            "image": image})
+    })
+    .then(res => res.json())
+    .then(data => {
+
+        elem = document.getElementById('dialog');
+        elem.classList.remove('hidden');
+        let element = document.createElement('p')
+
+        if (data['message'] === 'Food menu updated'){
+            document.getElementById('name').value = "";
+            document.getElementById('description').value = "";
+            document.getElementById('price').value = ""; 
+            document.getElementById('images').value = "";  
+                    
+            element.innerHTML =  "Food menu created";
+            element.id = "theoutput"
+            elem.appendChild(element)
+
+            setTimeout(() => {
+                location.reload();}, 1900);                                   
+             
+        }
+        else{
+            element.innerHTML =  `${data["message"]}`;
+            element.id = "newoutput"
+            elem.appendChild(element)
+  
+            setTimeout(() => {
+                location.reload();}, 1900);  
+
+            
+          }
+
+    })
+
+})
+}
+
     
 //function to delete a meal item
 function deleteMeal(id){
-    fetch(`https://createorders-api.herokuapp.com/api/v2/menu/${id}`,{
+    fetch(`${url}/${id}`,{
         method: 'DELETE',
         headers: {
             'Access-Control-Allow-Origin': '*',
